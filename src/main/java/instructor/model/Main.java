9c20df99f07aa97ee73941f63c782d4c87a37f6f
goodleaf96 <ky1234tho@gmail.com>
2025-01-14 16:40:46 +0900

    Instructor에서 main 삭제제

--- src/main/java/instructor/model/Main.java
+++
@@ -1,7 +0,0 @@
-package instructor.model;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
--- src/main/java/instructor/model/dao/Main.java
+++
@@ -1,7 +0,0 @@
-package instructor.model.dao;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
